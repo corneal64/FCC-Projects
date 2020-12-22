3e2f011f241b959c112500c902c898fdc750150f
Cornelius Edward <corneal64@gmail.com>
2020-12-22 10:25:02 +0600

    Update project

--- back-end/file-metadata/index.js
+++ back-end/file-metadata/index.js
@@ -1,40 +1,48 @@
-const express = require("express");
-const multer = require("multer");
-const fs = require("fs");
-const path = require("path");
+const express = require('express');
+const multer = require('multer');
+const fs = require('fs');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(multer({
-  dest: './uploads/'
-}).single("file"));
-app.set("view engine", "ejs");
+app.use(
+  multer({
+    dest: './uploads/'
+  }).single('upfile')
+);
 
-app.get("/", function (req, res) {
-  res.render("home");
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(process.cwd() + '/views/index.html');
 });
 
-app.post("/", function (req, res) {
+app.post('/api/fileanalyse', (req, res) => {
+  const { originalname, mimetype, size } = req.file;
+
   res.json({
-    filesize: req.file.size
+    name: originalname,
+    type: mimetype,
+    size: size
   });
+
   removeFiles();
 });
 
-app.listen(port, function () {
-  console.log(`Server Started on port ${port}`);
-});
-
 function removeFiles() {
-  const dir = "uploads";
+  const dir = 'uploads';
 
   fs.readdir(dir, (err, files) => {
     if (err) throw err;
 
     for (const file of files) {
-      fs.unlink(path.join(dir, file), err => {
+      fs.unlink(path.join(dir, file), (err) => {
         if (err) throw err;
       });
     }
   });
 }
+
+app.listen(port, function () {
+  console.log(`Server Started on port ${port}`);
+});
